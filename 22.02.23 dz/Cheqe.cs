7c90b63c39058f968e31404ae2f5ff5a6f93d653
Leshamir <anastasy34@gmail.com>
2023-02-27 02:00:41 +0200

    Добавьте файлы проекта.

---
+++ 22.02.23 dz/Cheqe.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _22._02._23_dz
+{
+    abstract public class Cheqe
+    {
+        private protected double money;
+        private protected string currency;
+        public Cheqe()
+        {
+            money = 0;
+            currency = null;
+        }
+        public Cheqe(double money, string currency)
+        {
+            this.money = money;
+            this.currency = currency;
+        }
+        abstract public string GetCheque();
+    }
+
+    public class NoCheque : Cheqe
+    {
+        public NoCheque(double money, string currency) : base(money, currency)
+        {
+        }
+
+        public override string GetCheque()
+        {
+            return null;
+        }
+    }
+
+    public class PaperCheque : Cheqe
+    {
+        public PaperCheque(double money, string currency) : base(money, currency)
+        {
+        }
+
+        public override string GetCheque()
+        {
+            return $"------CHEQUE------\nTotal:{money} {currency}";
+        }
+    }
+
+    public class ECheque : Cheqe
+    {
+        public ECheque(double money, string currency) : base(money, currency)
+        {
+        }
+
+        public override string GetCheque()
+        {
+            Console.WriteLine("Enter email.");
+            string mail = Console.ReadLine();
+            return $"------CHEQUE------\nTotal:{money} {currency}\nCopy was sent to {mail}";
+        }
+    }
+}
---
+++ 22.02.23 dz/Money.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _22._02._23_dz
+{
+    interface IPrint
+    {
+        void print();
+    }
+    class SentCheck : IPrint
+    {
+        public void print()
+        {
+            Console.WriteLine("Send check!");
+        }
+    }
+    class ShowCheck : IPrint
+    {
+        public void print()
+        {
+            Console.WriteLine("Show check");
+        }
+    }
+    class PrintCheck : IPrint
+    {
+        public void print()
+        {
+            Console.WriteLine("Print check");
+        }
+    }
+    interface IMoney
+    {
+        void give();
+        void receive();
+    }
+    class UAH : IMoney
+    {
+        public int uah;
+        public void give()
+        {
+            Console.WriteLine();
+        }
+        public void receive()
+        {
+            Console.WriteLine();
+        }
+    }
+}
